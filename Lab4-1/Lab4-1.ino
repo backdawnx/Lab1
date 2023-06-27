f9827953d5009cc9a93920bab9addd71f72f2bf1
backdawnx <you@example.c>
2023-06-27 15:22:09 +0700

    'update'

---
+++ Lab4-1/Lab4-1.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myservo;
+
+void setup() {
+
+  myservo.attach(9);
+
+}
+
+void loop() {
+
+  myservo.write(0);
+  delay(500);
+  myservo.write(90);
+  delay(500);
+  myservo.write(180);
+  delay(500);
+  myservo.write(360);
+  delay(500);
+}
